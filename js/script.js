0ae46fdb913cc6e79f7429a6b8cda763d81ec6d2
alecio sales <santosalecio03@gmail.com>
2021-09-27 00:04:09 -0300

    pasta js com o arquirvo js criado

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+ $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+  });
+
+  
+                  
