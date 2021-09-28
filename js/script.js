3957e18cc3adc79702a2e88617ee7dc1f49c61b6
alecio sales <santosalecio03@gmail.com>
2021-09-28 13:28:33 -0300

    textando menu

--- js/script.js
+++ js/script.js
@@ -8,5 +8,37 @@
     autoplaySpeed: 2000,
   });
 
+  $('.carousel_mobile').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+  });
+
+  $('.produtos-slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+  });
+
+  $(document).ready(function() {
+    $("body").on('click', '.hamburguer', function() {
+      $("nav.menu").toggleClass("exibe_menu");
+    });
+  });
+
+  function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+
+  }
+
+
   
                   
